d50a11fc83c83918aea501ac6dce18ba7b2ddce0
Lazzarotto-Leo <leolazzarotto@yahoo.fr>
2021-01-11 15:09:05 +0100

    CommitLeo

--- genconf-master/src/main/java/fr/uga/iut2/genconf/modele/Communication.java
+++ genconf-master/src/main/java/fr/uga/iut2/genconf/modele/Communication.java
@@ -5,10 +5,72 @@
  */
 package fr.uga.iut2.genconf.modele;
 
+import fr.uga.iut2.genconf.util.Type;
+
 /**
  *
  * @author leola
  */
 public class Communication {
+    
+    private String titre;
+    private Type type;
+    private Utilisateur correspondant;
+    private Session session;
+    
+    public static Communication initialiseCommunication(String titre, Type type, Utilisateur corres, Session session) {
+        Communication com = new Communication(titre, type, corres, session);
+        com.ajouterCorrespondant(corres);
+        return com;
+    }
+
+    public Communication(String titre, Type type,Utilisateur Correspondant, Session session) {
+        setTitre(titre);
+        setType(type);
+        setCorrespondant(Correspondant);
+        setSession(session);
+    }
+    
+    public void ajouterCorrespondant(Utilisateur corres){
+        corres.addCommunications(this);
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public Utilisateur getCorrespondant() {
+        return correspondant;
+    }
+
+    public void setCorrespondant(Utilisateur correspondant) {
+        this.correspondant = correspondant;
+    }
+
+    public Session getSession() {
+        return session;
+    }
+
+    public void setSession(Session session) {
+        this.session = session;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+    
+    public void update(){
+        
+    }
+    
+    
+    
+    
+    
+    
     
 }
